a8fc47cc70696e18bd8500c7697f681ed2f15931
Gayatri Suslade <gayatrisuslade@outlook.com>
2020-11-09 16:19:05 +0530

    First Commit

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ListComponent } from './list/list.component';
+console.warn("User Module");
+
+@NgModule({
+  declarations: [LoginComponent, ListComponent],
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
